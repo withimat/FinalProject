1d012be2e79f6cf489e5b9afabd5ac9cb0ca6702
withimat <imattgokk@gmail.com>
2025-04-24 13:35:19 +0300

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by İmat Gökaslan on 24.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
