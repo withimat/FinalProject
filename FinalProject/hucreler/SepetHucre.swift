eeb89f7d9da83e2a31101f51db3fe30747104993
withimat <imattgokk@gmail.com>
2025-05-04 15:16:38 +0300

    duzenlemeler yapıldı

---
+++ FinalProject/hucreler/SepetHucre.swift
@@ -0,0 +1,32 @@
+//
+//  SepetHucre.swift
+//  FinalProject
+//
+//  Created by İmat Gökaslan on 27.04.2025.
+//
+
+import UIKit
+protocol SepetHucreDelegate: AnyObject {
+    func urunuSil(sepetId: Int, kullaniciAdi: String)
+}
+
+class SepetHucre: UITableViewCell {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var LabelAd: UILabel!
+    @IBOutlet weak var LabelFiyat: UILabel!
+    @IBOutlet weak var LabelAdet: UILabel!
+    @IBOutlet weak var LabelToplamFiyat: UILabel!
+
+    weak var delegate: SepetHucreDelegate?
+    var sepetId: Int?
+    var kullaniciAdi = "imat"
+
+    @IBAction func TrashButton(_ sender: Any) {
+        if let id = sepetId {
+            
+            delegate?.urunuSil(sepetId: id, kullaniciAdi: kullaniciAdi)
+ 
+        }
+    }
+}
